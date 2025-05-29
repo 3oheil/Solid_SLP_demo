9833e09a66e174fc2ffe0aea25c47e72a36b0045
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-05-29 13:21:20 -0800

    test

--- Correc_example/Program.cs
+++ Correc_example/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<Bird> birds = new List<Bird>
+        {
+            new Eagle(),
+            new Ostrich()
+        };
+
+            foreach (var bird in birds)
+            {
+                bird.Eat();
+            }
+
+            List<IFyable> flyingBirds = new List<IFyable>
+        {
+            new Eagle()
+            // Ostrich is not here
+        };
+
+            foreach (var flyer in flyingBirds)
+            {
+                flyer.Fly(); // فقط پرنده‌هایی که واقعاً می‌تونن پرواز کنن
+            }
         }
     }
 }
