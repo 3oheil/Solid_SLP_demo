0c06deebf80627a2cffd880bb485b869ffec9a6e
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-05-29 13:10:32 -0800

    create eagle class

---
+++ LSP/Eagle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace violation_Example
+{
+    internal class Eagle : Bird
+    {
+        public override void Fly()
+        {
+            Console.WriteLine("The eagle soars high in the sky.");
+        }
+    }
+}
