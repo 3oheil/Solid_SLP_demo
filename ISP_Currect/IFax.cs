8a84caa0caa7d6ac6fe9e3f79840b8a16c601504
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-05-30 11:35:53 -0800

    design interface

--- ISP_Currect/IFax.cs
+++ ISP_Currect/IFax.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ISP_Currect
 {
-    internal interface IFax
+    public interface IFax
     {
+        void Fax(Document doc);
     }
 }
