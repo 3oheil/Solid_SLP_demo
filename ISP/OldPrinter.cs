82b5c7f05fb236b021fabf02fd46467389d5df0e
سهیل ایمانی اختیار <soheil81imani81@gmail.com>
2025-05-30 11:29:51 -0800

    create and impliment interface

---
+++ ISP/OldPrinter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISP
+{
+    internal class OldPrinter : IMultiFunctionMachine
+    {
+        public void Print(Document doc) => Console.WriteLine("Printing...");
+
+        public void Scan(Document doc)
+        {
+            throw new NotImplementedException(); // این دستگاه اصلاً قابلیت اسکن نداره!
+        }
+
+        public void Fax(Document doc)
+        {
+            throw new NotImplementedException(); // و نه حتی فکس
+        }
+    }
+}
